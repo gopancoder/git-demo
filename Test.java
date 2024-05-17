02d46dbe7ed3b8ef34b479461c4d90c3ea683740
gopancoder <gopanav5@gmail.com>
2024-05-17 14:38:33 +0530

    Derived classs added

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
-Class Demo{
+class Base{
 	public static void main(String [] args{
-		System.out.println("This is a Demo class");
+		System.out.println("This is a Base class");
 	}
 }
+
+class Derived extends Base{
+public static void main(String [] args{
+		System.out.println("This is a Derived class");
+	}
+}
+
+	
